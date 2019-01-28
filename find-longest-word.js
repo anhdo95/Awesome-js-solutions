0d93f0ff4cf1a43febc533c4b91d2ca37f31b4bb
anhdo <dinhanhdo95@gmail.com>
2019-01-28 21:20:39 +0700

    Find a longest word by using recursiveness

--- find-longest-word.js
+++ find-longest-word.js
@@ -1,9 +1,29 @@
+// function findLongestWordLength(sentence) {
+//   return sentence
+//     .split(' ')
+//     .reduce((previous, current) => Math.max(previous, current.length), 0)
+// }
+
+/**
+ * Using recursiveness
+ * @param {string} sentence 
+ */
 function findLongestWordLength(sentence) {
-  return sentence
-    .split(' ')
-    .reduce((previous, current) => Math.max(previous, current.length), 0)
+  const strings = sentence.split(' ')
+
+  if (strings.length === 1) 
+    return strings[0].length
+
+  if (strings[0].length < strings[1].length)
+    return findLongestWordLength(strings
+      .slice(1, strings.length)
+      .join(' ')
+    )
+
+  strings.splice(1, 1)
+  return findLongestWordLength(strings.join(' '))
 }
 
-const result = findLongestWordLength('check for the longest word by comparing the current word to the previous one');
+const result = findLongestWordLength('check for the longest word by comparing the current word to the previous one')
 
-console.log(result); // 9
+console.log(result) // 9
