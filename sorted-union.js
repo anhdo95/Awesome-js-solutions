f40a4255096f67fd78d34d5546f8c3140b3da10e
anhdo <dinhanhdo95@gmail.com>
2019-02-03 15:03:25 +0700

    Unites unique arrays by using a 'Set' object

--- sorted-union.js
+++ sorted-union.js
@@ -3,14 +3,26 @@
  * @param {array} original 
  * @param  {...array} arrays 
  */
-function uniteUnique(original, ...arrays) {
-  if (arrays.length === 1) {
-    const uniqueItems = arrays[0].filter(item => !original.includes(item))
+// function uniteUnique(original, ...arrays) {
+//   if (arrays.length === 1) {
+//     const uniqueItems = arrays[0].filter(item => !original.includes(item))
 
-    return original.concat(uniqueItems)
-  }
+//     return original.concat(uniqueItems)
+//   }
 
-  return arrays.reduce((previous, array) => uniteUnique(previous, array), original)
+//   return arrays.reduce((previous, array) => uniteUnique(previous, array), original)
+// }
+
+/**
+ * Use a Set object to create an unique value array
+ * @param  {...array} arrays 
+ */
+function uniteUnique(...arrays) {
+  const flattenArray = [].concat(...arrays)
+
+  // The Set object lets you store unique values of any type,
+  // whether primitive values or object references.
+  return [ ...new Set(flattenArray) ]
 }
 
-uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 3, 2], [10, 2, 1, 4], [2, 11, 3])
