a76638ffeeb4421da427d37662ea41286588516e
anhdo <dinhanhdo95@gmail.com>
2019-02-04 17:17:48 +0700

    Flatten a nested array for an intermediate solution

--- flatten-nested-array.js
+++ flatten-nested-array.js
@@ -1,17 +1,28 @@
 /**
  * Flatten a nested array. Which has varying levels of nesting
+ * (BASIC SOLUTION)
+ * @param {array} array 
+ */
+// function steamrollArray(array) {
+//   const flattenArray = []
+
+//   const flatten = (arg) => !Array.isArray(arg) 
+//     ? flattenArray.push(arg)
+//     : arg.forEach(flatten)
+
+//   array.forEach(flatten)
+
+//   return flattenArray
+// }
+
+/**
+ * Flatten a nested array. Which has varying levels of nesting
+ * (INTERMEDIATE SOLUTION)
  * @param {array} array 
  */
 function steamrollArray(array) {
-  const flattenArray = []
-
-  const flatten = (arg) => !Array.isArray(arg) 
-    ? flattenArray.push(arg)
-    : arg.forEach(flatten)
-
-  array.forEach(flatten)
-
-  return flattenArray
+  const flatten = [].concat(...array)
+  return flatten.some(Array.isArray) ? steamrollArray(flatten) : flatten
 }
 
-steamrollArray([0, [9], [3, 4, [[10]]]])
+steamrollArray([1, [9], [3, 4, [[10]]]])
