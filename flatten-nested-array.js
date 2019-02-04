29488f8456efbe164809c6009757da924634db75
anhdo <dinhanhdo95@gmail.com>
2019-02-04 18:02:15 +0700

    Flatten a nested array for an advanced solution

--- flatten-nested-array.js
+++ flatten-nested-array.js
@@ -20,9 +20,26 @@
  * (INTERMEDIATE SOLUTION)
  * @param {array} array 
  */
+// function steamrollArray(array) {
+//   const flatten = [].concat(...array)
+//   return flatten.some(Array.isArray) ? steamrollArray(flatten) : flatten
+// }
+
+/**
+ * Flatten a nested array. Which has varying levels of nesting
+ * (ADVANCED SOLUTION)
+ * @param {array} array 
+ */
 function steamrollArray(array) {
-  const flatten = [].concat(...array)
-  return flatten.some(Array.isArray) ? steamrollArray(flatten) : flatten
+  // double comma if there was '[]' and ''
+  return array.toString().replace(/,,/g, ',').split(',').map(getActualValue)
 }
 
-steamrollArray([1, [9], [3, 4, [[10]]]])
+function getActualValue(value) {
+  console.log(value);
+  if (value === '[object Object]') return {}
+  if (isNaN(value) || value === '') return value
+  return +value
+}
+
+steamrollArray([1, '', [{}, []], {}, '', [3, [[4]]]]);
